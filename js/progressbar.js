36a0cb8d9bed32439fa6fff20e3f759dd99f0b00
Joonkun Yoon <joongun2001@naver.com>
2017-08-09 10:51:24 +0000

    first commit

---
+++ js/progressbar.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){ 
+	$(window).scroll(function() { 
+		if ($(document).scrollTop()>200) { 
+				  $("#myBar1").animate({width:"70%"},2000);
+		}
+	});
+});
+$(document).ready(function(){ 
+	$(window).scroll(function() { 
+		if ($(document).scrollTop()>200) { 
+				  $("#myBar2").animate({width:"60%"},2000);
+		}
+	});
+});
+$(document).ready(function(){ 
+	$(window).scroll(function() { 
+		if ($(document).scrollTop()>200) { 
+				  $("#myBar3").animate({width:"20%"},2000);
+		}
+	});
+});
+$(document).ready(function(){ 
+	$(window).scroll(function() { 
+		if ($(document).scrollTop()>200) { 
+				  $("#myBar4").animate({width:"10%"},2000);
+		}
+	});
+});
+$(document).ready(function(){ 
+	$(window).scroll(function() { 
+		if ($(document).scrollTop()>1000) { 
+				  $("#myBar5").animate({width:"70%"},2000);
+		}
+	});
+});
+$(document).ready(function(){ 
+	$(window).scroll(function() { 
+		if ($(document).scrollTop()>1000) { 
+				  $("#myBar6").animate({width:"20%"},2000);
+		}
+	});
+});
+$(document).ready(function(){ 
+	$(window).scroll(function() { 
+		if ($(document).scrollTop()>1000) { 
+				  $("#myBar7").animate({width:"10%"},2000);
+		}
+	});
+});
